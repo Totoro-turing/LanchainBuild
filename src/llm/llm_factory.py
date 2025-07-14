bee6b6062c2b2db20dc5830ea41b9df38a2aa01f
longyu3 <18030556662@163.com>
2025-07-14 12:21:50 +0800

    1.1

---
+++ src/llm/llm_factory.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
+load_dotenv()
+class LLMFactory:
+    @staticmethod
+    def create_llm() -> ChatOpenAI:
+        return ChatOpenAI(
+            temperature=0,
+            model="deepseek-chat",
+            api_key=os.getenv('DEEPSEEK_API_KEY'),
+            base_url=os.getenv('DEEPSEEK_BASE_URL')
+        )    
